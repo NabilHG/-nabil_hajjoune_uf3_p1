dbb4b8a290e9b129f3454a7f9eadc132481e0a9c
NabilHG <nabilhajjoune@gmail.com>
2024-01-24 18:56:59 +0100

    #2 created new field, agency at film table

--- database/migrations/2024_01_17_172812_create_films_table.php
+++ database/migrations/2024_01_17_172812_create_films_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('country', 30);
             $table->integer('duration');
             $table->string('img_url', 255);
+            $table->string('agencia', 100); // New field 'agencia' with max lengh 100 char
             $table->timestamps();
         });
     }
