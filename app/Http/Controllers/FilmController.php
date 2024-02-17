730b856f4dc16d035bdc404627b4cf54f1660191
nabil.hajjoune <nabil.hajjoune@pistacero.net>
2024-02-17 12:33:37 +0100

    done some work in flow work, view, web.php and controllers

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -227,4 +227,62 @@ class FilmController extends Controller
             return view('films.list', ["films" => $films, "title" => $title]);
         }
     }
+
+    /**
+     * List all actors in the database
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function listActors()
+    {
+
+        $title = "List of all actors";
+        $actors = DB::table('actors')->get();
+
+        // dd($actors); // Remove this unless for debugging
+
+        return view('actors.list', ["actors" => $actors, "title" => $title]);
+    }
+
+    /**
+     * List all actors by decade
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function listActorsByDecade($year = null)
+    {
+
+        $title = "List actors from ".$year." decade";
+        $actors = DB::table('actors')->get()->where();
+
+        // dd($actors); // Remove this unless for debugging
+
+        return view('actors.list', ["actors" => $actors, "title" => $title]);
+    }
+
+    /**
+     * Count the number of actors.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function countActors()
+    {
+        $title = "Number of Actors";
+
+        $actors = count(DB::table('actors')->get());
+
+        return view('actors.count', ["actors" => $actors, "title" => $title]);
+    }
+
+    /**
+     * Delete an actor from database by id.
+     *
+     * @return json
+     */
+    public function deleteActor($id){
+
+        $status = '';
+
+        return response()->json(['action' => 'delete', 'status' => $status]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,13 @@ Route::middleware('url')->group(function () {
         Route::post('createFilm', [FilmController::class, "createFilm"])->name('createFilm');
     });
 });
+
+
+//Actor's routes
+Route::group(['prefix' => 'actorout'], function () {
+    // Routes included with prefix "actorout"
+    Route::get('actors', [FilmController::class, "listActors"])->name('listActors');
+    Route::get('listActorsByDecade/{year}', [FilmController::class, "listActorsByDecade"])->name('listActorsByDecade');
+    Route::get('countActors', [FilmController::class, "countFilms"])->name('countActors');
+    Route::get('deleteActor/{id}', [FilmController::class, "deleteActor"])->name('deleteActor');
+});
